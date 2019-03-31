0994e940fb9f8730542ef2833d436d882ea2360a
Clayton Conly <claytonconly@gmail.com>
2019-03-30 19:51:02 -0500

    adjusting url name

--- website/action.php
+++ website/action.php
@@ -3,7 +3,7 @@
         if (isset($_POST['submit'])){
             $id = $_POST['id'];
         }
-            $apiurl="http://ec2-18-223-239-89.us-east-2.compute.amazonaws.com:8080/checkin/" . $id;
+            $apiurl="http://ec2-18-223-239-89.us-east-2.compute.amazonaws.com:8080/checkin/log-checkin/" . $id;
 
             $response = file_get_contents($apiurl);
             echo $response;
