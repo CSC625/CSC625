1b446361d87f426e1166416c8833861f595620ab
Clayton Conly <claytonconly@gmail.com>
2019-03-30 19:28:17 -0500

    working to call with input

--- website/action.php
+++ website/action.php
@@ -1,8 +1,14 @@
 <?php
-		$id=$POST['ID'];
-		//add api url 
-		$apiurl="http://ec2-18-223-239-89.us-east-2.compute.amazonaws.com:8080/checkin/all-users" . $id;
 
-		$response = file_get_contents($apiurl);
-		echo $response;
+        if (isset($_POST['submit'])){
+            $id = $_POST['id'];
+        }
+            $apiurl="http://ec2-18-223-239-89.us-east-2.compute.amazonaws.com:8080/checkin/" . $id;
+
+            $response = file_get_contents($apiurl);
+            echo $response;
+
+                //$id=$_POST['submit'];
+                //add api url
+
 ?>
