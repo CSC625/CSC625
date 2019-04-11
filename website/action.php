9dcad334db525fb620977d442b6601edaa0e0a37
Clayton Conly <claytonconly@gmail.com>
2019-04-11 16:27:49 -0500

    updated php file

--- website/action.php
+++ website/action.php
@@ -1,10 +1,35 @@
 <?php
+
         if (isset($_POST['submit'])){
             $id = $_POST['id'];
+            echo $id;
         }
+
+
+
+
+
             $apiurl="http://ec2-18-223-239-89.us-east-2.compute.amazonaws.com:8080/checkin/log-checkin/" . $id;
-            $response = file_get_contents($apiurl);
+            $ch = curl_init( $apiurl );
+            curl_setopt( $ch, CURLOPT_POST, 1);
+            curl_setopt( $ch, CURLOPT_POSTFIELDS, $id);
+            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt( $ch, CURLOPT_HEADER, 0);
+            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+
+
+            $response = curl_exec( $ch );
+
+
+
+
+
+
+
             echo $response;
+
                 //$id=$_POST['submit'];
                 //add api url
+
 ?>
